e3b7b836e3549fc14dab0d102aef8ec169490777
ALTENALT <95972475+ALTENALT@users.noreply.github.com>
2024-11-07 23:18:56 +0300

    Proje dosyası ekle.

---
+++ GPT_Browser_Ehe/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace GPT_Browser_Ehe
+{
+    partial class ChatGPT
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChatGPT));
+            this.webView2 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView2
+            // 
+            this.webView2.AllowExternalDrop = true;
+            this.webView2.CreationProperties = null;
+            this.webView2.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView2.Location = new System.Drawing.Point(0, 0);
+            this.webView2.Name = "webView2";
+            this.webView2.Size = new System.Drawing.Size(800, 450);
+            this.webView2.TabIndex = 0;
+            this.webView2.ZoomFactor = 1D;
+            // 
+            // ChatGPT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webView2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ChatGPT";
+            this.Text = "ChatGPT";
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView2;
+    }
+}
+
---
+++ GPT_Browser_Ehe/Form1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Windows.Forms;
+
+namespace GPT_Browser_Ehe
+{
+    public partial class ChatGPT : Form
+    {
+        public ChatGPT()
+        {
+            InitializeComponent();
+            InitializeBrowser();
+        }
+
+        private async void InitializeBrowser()
+        {
+            await webView2.EnsureCoreWebView2Async(null);
+            webView2.Source = new Uri("https://chatgpt.com");
+        }
+
+        private void webView2_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            if (e.Uri != "https://chatgpt.com")
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
+// Bu yazılım, kişisel ve ticari olmayan projelerde özgürce kullanılabilir, değiştirilebilir ve dağıtılabilir. Ancak, bu yazılımın ticari olarak kullanılması, dağıtılması veya satış amaçlı türevlerinin oluşturulması durumunda, izin almak için [myusufaksu2001@hotmail.com] ile iletişime geçilmelidir.
